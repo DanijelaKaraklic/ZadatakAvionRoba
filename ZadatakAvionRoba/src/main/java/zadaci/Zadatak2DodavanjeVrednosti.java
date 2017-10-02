2a8488f3c04d01832c6f268a66bf738853899923
Danijela <danijela.popovic987@gmail.com>
2017-10-02 20:26:56 +0200

    Uradjen zadatak dodavanje vrednosti fixes #5

--- ZadatakAvionRoba/src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
+++ ZadatakAvionRoba/src/main/java/zadaci/Zadatak2DodavanjeVrednosti.java
@@ -1,16 +1,68 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
+import java.util.List;
+
 
 public class Zadatak2DodavanjeVrednosti {
+    static Dao<Avion,Integer> avionDao;
+    static Dao<Roba,Integer> robaDao;
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
 
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+            TableUtils.clearTable(connectionSource,Avion.class);
+            TableUtils.clearTable(connectionSource,Avion.class);
+
+            avionDao = DaoManager.createDao(connectionSource,Avion.class);
+            robaDao = DaoManager.createDao(connectionSource,Roba.class);
+
+            Avion a1 = new Avion("Avion1","34");
+            avionDao.create(a1);
+            Avion a2 = new Avion("Avion2","21");
+            avionDao.create(a2);
+
+            Roba r1 = new Roba("Patike","Duboke patike",1);
+            r1.setAvion(a1);
+            robaDao.create(r1);
+
+            Roba r2 = new Roba("Kosulja","Na dugacke rukave",0.4);
+            r2.setAvion(a1);
+            robaDao.create(r2);
+
+            Roba r3 = new Roba("Voda","Voda za pice",1.4);
+            r3.setAvion(a1);
+            robaDao.create(r1);
+
+            Roba r4 = new Roba("Ploce","Drvene ploce",3.4);
+            r4.setAvion(a2);
+            robaDao.create(r1);
+
+            Roba r5 = new Roba("Stolica","Plasticna stolica",2.4);
+            r5.setAvion(a2);
+            robaDao.create(r1);
+
+            List<Avion> avioni = avionDao.queryForAll();
+            for (Avion a:avioni) {
+                System.out.println(a);
+            }
+
+            List<Roba> robe = robaDao.queryForAll();
+            for (Roba a:robe) {
+                System.out.println(a);
+            }
+
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
