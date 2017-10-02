a60cb46f177542f9d23147c064c42385a5909d98
Danijela <danijela.popovic987@gmail.com>
2017-10-02 20:54:23 +0200

    Uradjen zadatak brisanja vrednosti fixes #7

--- ZadatakAvionRoba/src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ ZadatakAvionRoba/src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,17 +1,41 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Zadatak4BrisanjeVrednosti {
+    static Dao<Avion,Integer> avionDao;
+    static Dao<Roba,Integer> robaDao;
     public static void main(String[] args) {
         ConnectionSource connectionSource = null;
         try {
 
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+            avionDao = DaoManager.createDao(connectionSource,Avion.class);
+            robaDao = DaoManager.createDao(connectionSource,Roba.class);
+
+            List<Roba> robe = robaDao.queryForAll();
+            for (Roba a:robe) {
+                System.out.println(a);
+            }
+            robe = robaDao.queryForEq(Roba.POLJE_NAZIV,"Voda");
+            Roba roba = robe.get(0);
+            robaDao.delete(roba);
+            for (Roba a:robe) {
+                System.out.println(a);
+            }
+
+
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
